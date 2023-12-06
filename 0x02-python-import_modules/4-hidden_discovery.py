2d695a8b96b916fa25c52ce6831aef5e55c980c4
wayneecleo <wayneecleo@gmail.com>
2023-12-06 13:17:31 -0800

    Hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/bin/usr/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-    if allf [i] [:2] =! "__":
-            print(" {:s}".format(allf[i]))
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
